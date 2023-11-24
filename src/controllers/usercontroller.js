987cd89b9afe7fcb410d7b87169cab0a23ec7bdb
Ally-3 <146335458+Ally-3@users.noreply.github.com>
2023-11-24 16:12:44 +0000

    token assigned to login

--- src/controllers/usercontroller.js
+++ src/controllers/usercontroller.js
@@ -35,10 +35,22 @@ async function register(req, res) {
 
 // LOGIN
 async function login(req, res) {
+    const expirationTime = 1000*60*60*24*7
+    const privateKey = process.env.JWTPASSWORD
+    const payload = {
+        email : req.body.email
+    };
+    
+    const options = {
+        expiresIn : expirationTime
+    };
+    const token = await jwt.sign(payload, privateKey, options);
+    console.log(token);
     try {
         res.status(201).json({
             message : "user logged in",
-            user : req.body.email
+            user : req.body.email,
+            token: token
         })
     } catch (error) {
         res.status(500).json({
